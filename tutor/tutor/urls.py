d08ce5526066dd5b33adbe9862c811c62e72b561
greentealemonade <kaydeee329@gmail.com>
2020-07-06 16:20:40 +0900

    added main page template

--- tutor/tutor/urls.py
+++ tutor/tutor/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from matching import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main1'),
 ]
