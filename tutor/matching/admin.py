173c4d606dde451491b91829367be2c2126ecf38
yejineee <21700431@handong.edu>
2020-07-07 16:48:01 +0900

    signup form
    Signed-off-by: yejineee <21700431@handong.edu>

--- tutor/matching/admin.py
+++ tutor/matching/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Post, Comment, Topic, Report
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Topic)
+admin.site.register(Report)
+admin.site.register(Comment)
--- tutor/matching/views.py
+++ tutor/matching/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
-from .models import Post
+from .models import User, Post
+from .forms import SignupForm
+
+
+
 class IndexView(generic.ListView):
     template_name = 'matching/main.html'
     def get_queryset(self):
         #returns the last five published questions
         return Post.objects.order_by('-pub_date')[:5]
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            return redirect('/matching') # redirect으로 tutee home으로 이동
+    else:
+        form = SignupForm()
+    return render(request, 'matching/signup.html', {
+        'form' : form,
+    })
+
