0bd85eb1405505d9512d70d8140f0243f16a7e6b
sungminkim <ksm3169@gmail.com>
2020-07-08 17:14:18 +0900

    Change post page

--- tutor/matching/forms.py
+++ tutor/matching/forms.py
@@ -5,16 +5,12 @@ from django.forms import ModelChoiceField
 class SignupForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'student_number','nickname', 'phone', 'email'] 
+        fields = ['name', 'student_number','nickname', 'phone', 'email']
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('topic', 'title', 'content')
-
-    def __init__(self, *args, **kwargs):
-        super(PostForm, self).__init__(*args, **kwargs)
-        self.fields['topic']=forms.ModelChoiceField(queryset=Topic.objects.all())
+        fields = ['title', 'content']
 
 class ReportForm(forms.ModelForm):
     class Meta:
