697035f0459cc18e7af9867c398d9aca477766a2
yejineee <21700431@handong.edu>
2020-07-10 15:40:07 +0900

    report
    Signed-off-by: yejineee <21700431@handong.edu>

--- tutor/matching/forms.py
+++ tutor/matching/forms.py
@@ -23,15 +23,8 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('content',)
 
-<<<<<<< HEAD
 class AcceptReportForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = ('tutee_feedback',)
 
-=======
-class ReportForm(forms.ModelForm):
-    class Meta:
-        model = Report
-        fields = ('meeting_date','meeting_duration_time','content')
->>>>>>> e68da6f225cbd6fa63084bc2c7e11e95dbac315d
