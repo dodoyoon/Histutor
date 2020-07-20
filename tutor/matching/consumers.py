23ea284f4e1b51c11624d9c4ce5450b846574f0b
greentealemonade <kaydeee329@gmail.com>
2020-07-20 16:54:17 +0900

    websocket fix - delete topic related code

--- tutor/matching/consumers.py
+++ tutor/matching/consumers.py
@@ -51,6 +51,67 @@ class NewPostConsumer(WebsocketConsumer):
         title = event['title']
         finding = event['finding']
         pub_date = event['pub_date']
+        nickname = event['nickname']
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'type': 'new_post',
+            'id': id,
+            'title': title,
+            'finding': finding,
+            'pub_date': pub_date,
+            'nickname': nickname,
+        }))
+
+class PostDetailConsumer(WebsocketConsumer):
+    def connect(self):
+        self.group_name = 'new_post'
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        id = text_data_json['id']
+        title = text_data_json['title']
+        finding = text_data_json['finding']
+        pub_date = text_data_json['pub_date']
+        topic = text_data_json['topic']
+        nickname = text_data_json['nickname']
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            {
+                'type': 'new_post',
+                'id': id,
+                'title': title,
+                'finding': finding,
+                'pub_date': pub_date,
+                'topic': topic,
+                'nickname': nickname,
+            }
+        )
+
+    # Receive message from room group
+    def new_post(self, event):
+        id = event['id']
+        title = event['title']
+        finding = event['finding']
+        pub_date = event['pub_date']
         topic = event['topic']
         nickname = event['nickname']
 
