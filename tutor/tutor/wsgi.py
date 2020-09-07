5786ab1b283a5243879327b5be5206ab5a125d0b
Sung-Min-Kim <ksm3169@gmail.com>
2020-09-07 21:39:17 +0900

    jquery is served from CDN

--- tutor/tutor/wsgi.py
+++ tutor/tutor/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+#sys.path.append('/home/team/tutor_match/tutor')
+#sys.path.append('/home/team/tutor_match/tutor/matching')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutor.settings')
 
 application = get_wsgi_application()
