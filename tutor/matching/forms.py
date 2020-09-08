5a85034773bfd176dd479e266793bdf9ca17f35d
dodoyoon <kaydeee329@gmail.com>
2020-09-08 17:16:37 +0900

    placeholder message changed

--- tutor/matching/forms.py
+++ tutor/matching/forms.py
@@ -10,7 +10,7 @@ class AccuseForm(forms.ModelForm):
         fields = ['join_tutee', 'content']
 
 class PostForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'size': '50', 'placeholder': '예) MySQL 설치하는데 자꾸 오류가 떠요', 'minlength': '15'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'size': '50', 'placeholder': '예) OS에 Deadlock에 대해서 도움이 필요합니다.', 'minlength': '15'}))
     class Meta:
         model = Post
         fields = ['title']
