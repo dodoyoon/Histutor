05bf7a9e3c50aec640c80969f6d28a2a770574e9
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-09-13 18:11:20 +0900

    Add websocket urls for local usage In local, the websocket url does not use the port 8443. Leave both url for local and server to use it more easily

--- tutor/tutor/settings.py
+++ tutor/tutor/settings.py
@@ -137,7 +137,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
-SITE_ID = 17
+SITE_ID = 12
 
 ACCOUNT_LOGOUT_REDIRECT_URL ="/matching"
 LOGIN_REDIRECT_URL = 'matching:user_check'
@@ -161,8 +161,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("redis://:AledmaThakd@172.17.0.2:6379/0")],
-            # "hosts": [('127.0.0.1', 6379)],
+            # "hosts": [("redis://:AledmaThakd@172.17.0.2:6379/0")],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
